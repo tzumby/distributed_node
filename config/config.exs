01c12f73858afbe1ebe99fdd3364c6e5f790c05f
Razvan Draghici <tzumby@gmail.com>
2023-07-31 21:32:06 -0400

    Debug false

--- config/config.exs
+++ config/config.exs
@@ -4,3 +4,5 @@ config :ex_aws,
   debug_requests: false,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+
+config :distributed_node, :debug, false
