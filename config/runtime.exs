fc76b7322486e578086c5d424041d5a05c846d1a
Raz <tzumby@gmail.com>
2023-08-08 16:43:22 +0000

    Update

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,13 @@
 import Config
 
 if config_env() == :prod do
+
+  node_name =
+    System.get_env("NODE_NAME") ||
+      raise """
+      NODE_NAME environment variable is not set.
+      """
+
+  config :partisan, :name, node_name |> String.to_atom()
+
 end
