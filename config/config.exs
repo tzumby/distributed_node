1438a697164babf2b552a1b334ae799feac24103
Razvan Draghici <tzumby@gmail.com>
2023-08-30 10:57:02 -0400

    Bring back default shuffle_interval

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,13 @@ config :ex_aws,
 config :partisan, :membership_strategy, :partisan_scamp_v2_membership_strategy
 config :partisan, :peer_service_manager, :partisan_hyparview_peer_service_manager
 config :partisan, :peer_port, 41234
-config :partisan, :hyparview, random_promotion: false, shuffle_interval: 100_000
+
+config :partisan, :hyparview,
+  # active_max_size: 10,
+  # active_rwl: 10,
+  # passive_rwl: 5,
+  # active_min_size: 5,
+  random_promotion: false,
+  shuffle_interval: 1000
 
 import_config "#{config_env()}.exs"
