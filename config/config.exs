51d975b43f9bf4ee308af72fd2c06cc0ad96640f
Razvan Draghici <tzumby@gmail.com>
2023-08-30 21:53:36 -0400

    make pertisan configuration at runtime possible

--- config/config.exs
+++ config/config.exs
@@ -5,18 +5,18 @@ config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
 
-config :partisan, :membership_strategy, :partisan_scamp_v2_membership_strategy
-config :partisan, :channels, [{:pubsub, %{parallelism: 10}}]
-config :partisan, :peer_service_manager, :partisan_hyparview_peer_service_manager
-config :partisan, :peer_port, 41234
-
-config :partisan, :hyparview,
-  active_max_size: 100,
-  active_rwl: 20,
-  passive_rwl: 10,
-  active_min_size: 50,
-  random_promotion: false,
-  shuffle_interval: 1000
+# config :partisan, :membership_strategy, :partisan_scamp_v2_membership_strategy
+# config :partisan, :channels, [{:pubsub, %{parallelism: 10}}]
+# config :partisan, :peer_service_manager, :partisan_hyparview_peer_service_manager
+# config :partisan, :peer_port, 41234
+#
+# config :partisan, :hyparview,
+#  active_max_size: 100,
+#  active_rwl: 20,
+#  passive_rwl: 10,
+#  active_min_size: 50,
+#  random_promotion: false,
+#  shuffle_interval: 1000
 
 # config :logger,
 #  # or other Logger level
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,8 @@
 import Config
 
+def to_boolean("true"), do: true
+def to_boolean("false"), do: false
+
 if config_env() == :prod do
   node_name =
     System.get_env("NODE_NAME") ||
@@ -13,7 +16,35 @@ if config_env() == :prod do
       RABBITMQ_URL environment variable is not set.
       """
 
+  active_max_size = System.get_env("ACTIVE_MAX_SIZE") || "100"
+  active_rwl = System.get_env("ACTIVE_RWL") || "20"
+  passive_rwl = System.get_env("PASSIVE_RWL") || "10"
+  active_min_size = System.get_env("ACTIVE_MIN_SIZE") || "50"
+  random_promotion = System.get_env("RANDOM_PROMOTION") || "false"
+  shuffle_interval = System.get_env("SHUFFLE_INTERVAL") || "1000"
+
+  peer_port = System.get_env("PEER_PORT") || "41234"
+
+  membership_strategy =
+    System.get_env("MEMBERSHIP_STRATEGY") || "partisan_scamp_v2_membership_strategy"
+
+  peer_service_manager =
+    System.get_env("PEER_SERVICE_MANAGER") || "partisan_hyparview_peer_service_manager"
+
   config :distributed_node, :rabbitmq_url, rabbitmq_url
 
   config :partisan, :name, node_name |> String.to_atom()
+
+  config :partisan, :hyparview,
+    active_max_size: active_max_size |> String.to_integer(),
+    active_rwl: active_rwl |> String.to_integer(),
+    passive_rwl: passive_rwl |> String.to_integer(),
+    active_min_size: active_min_size |> String.to_integer(),
+    random_promotion: random_promotion |> to_boolean(),
+    shuffle_interval: shuffle_interval |> to_boolean()
+
+  config :partisan, :peer_port, String.to_integer(peer_port)
+
+  config :partisan, :membership_strategy, String.to_atom(membership_strategy)
+  config :partisan, :peer_service_manager, String.to_atom(peer_service_manager)
 end
