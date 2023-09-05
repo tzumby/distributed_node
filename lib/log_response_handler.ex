bc5bde6a6e991ba21c5f32289446dcd74a76ac24
Razvan Draghici <tzumby@gmail.com>
2023-09-05 11:48:42 -0400

    Remove sensitive data

--- lib/log_response_handler.ex
+++ lib/log_response_handler.ex
@@ -7,10 +7,6 @@ defmodule LogResponseHandler do
         metadata,
         _config
       ) do
-    #  Event: [:worker, :processing, :stop]
-    #  Measurements: %{duration: 2571621, monotonic_time: -576460407581461477}
-    #  Metadata: %{node: :"a@Razvans-MacBook-Pro-2", result: :hey_back, telemetry_span_context: #Reference<0.1191962707.377749506.193778>}
-
     name = event |> Enum.join("-")
 
     payload = %{
@@ -25,21 +21,10 @@ defmodule LogResponseHandler do
     publish_telemetry_metric(payload)
   end
 
-  def handle_event(_event, _measurements, _metadata, _config) do
-    #  Event: [:worker, :processing, :start]
-    #  Measurements: %{monotonic_time: -576460407584033098, system_time: 1691092298148313902}
-    #  Metadata: %{node: :"a@Razvans-MacBook-Pro-2", telemetry_span_context: #Reference<0.1191962707.377749506.193778>}
-
-    # Logger.info("Event: #{inspect(event)}")
-    # Logger.info("Measurements: #{inspect(measurements)}")
-    # Logger.info("Metadata: #{inspect(metadata)}")
-  end
-
   def publish_telemetry_metric(payload) do
-    {:ok, conn} =
-      AMQP.Connection.open(
-        "amqps://admin:87!KCDcMG9nL*pLD@b-69264c08-8410-49f7-81af-aeea3607b590.mq.us-east-2.amazonaws.com:5671"
-      )
+    url = Application.get_env(:distributed_node, :rabbitmq_url)
+
+    {:ok, conn} = AMQP.Connection.open(url)
 
     {:ok, channel} = AMQP.Channel.open(conn)
 
