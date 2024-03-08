d72f2b0859b49ab14fe63f60df1ce208f52807bc
Razvan Draghici <tzumby@gmail.com>
2024-03-08 08:37:37 -0400

    fixup

--- lib/distributed_node/application.ex
+++ lib/distributed_node/application.ex
@@ -12,13 +12,15 @@ defmodule DistributedNode.Application do
     start_distribution()
 
     topologies = [
-      gossip: [
+      gossip_example: [
         strategy: Cluster.Strategy.Gossip,
         config: [
           port: 45892,
           if_addr: "0.0.0.0",
-          multicast_addr: "224.0.0.10",
-	  secret: "somepassword"]]
+          multicast_addr: "255.255.255.255",
+          broadcast_only: true
+        ]
+      ]
     ]
 
     children = [
@@ -36,25 +38,25 @@ defmodule DistributedNode.Application do
 
   def start_distribution do
     :application.set_env(:kernel, :net_ticker_spawn_options, [{:fullsweep_after, 0}])
-    :application.set_env(:kernel, :inet_dist_listen_options, [
-	{:high_watermark, 16777216},
-        {:low_watermark, 14680064},
-	{:nodelay, true},
-	{:reuseaddr, true},
-	{:debug, false},
-	{:buffer, 5000000},
-	{:active, false},
-	{:backlog, 4096}
-   ])
 
-    :application.set_env(:kernel, :inet_dist_connect_options, [
-	{:high_watermark, 16777216},
-        {:low_watermark, 14680064},
-	{:debug, false},
-	{:nodelay, true},
-	{:buffer, 5000000},
+    :application.set_env(:kernel, :inet_dist_listen_options, [
+      {:high_watermark, 16_777_216},
+      {:low_watermark, 14_680_064},
+      {:nodelay, true},
+      {:reuseaddr, true},
+      {:debug, false},
+      {:buffer, 5_000_000},
+      {:active, false},
+      {:backlog, 4096}
     ])
 
+    :application.set_env(:kernel, :inet_dist_connect_options, [
+      {:high_watermark, 16_777_216},
+      {:low_watermark, 14_680_064},
+      {:debug, false},
+      {:nodelay, true},
+      {:buffer, 5_000_000}
+    ])
 
     :net_kernel.start(node_name(), %{name_domain: :longnames})
   end
@@ -73,7 +75,7 @@ defmodule DistributedNode.Application do
       )
   end
 
-  def node_name do 
-    Application.get_env(:distributed_node, :node_name, "app") |> String.trim() |> String.to_atom()  
-  end 
+  def node_name do
+    Application.get_env(:distributed_node, :node_name, "app") |> String.trim() |> String.to_atom()
+  end
 end
